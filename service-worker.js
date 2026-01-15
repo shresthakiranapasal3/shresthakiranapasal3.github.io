4a53a95479530259b290a61c5a52b5f2e40f2b31
shresthakiranapasal <shresthakiranapasal3@gmail.com>
2026-01-16 00:31:14 +0545

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,23 @@
-self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+const CACHE_NAME = "kirana-v1";
+const FILES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/logo.png",
+  "/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
